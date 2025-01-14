f31c43bc9a771877a6009050b5760ce4c4a64fb9
Gideon Levi <gdeonlev@gmail.com>
2025-01-14 19:34:30 +0700

    finished 2591

---
+++ problems/2591-Distribute_Money_to_Maximum_Children/main.py
@@ -0,0 +1,23 @@
+class Solution:
+    def distMoney(self, money: int, children: int) -> int:
+        if children > money:
+            return -1
+        money = money - children
+        count = money // 7
+        
+        if children < count:
+            count = children
+            remainder = (money - children*7) 
+        else:
+            remainder = (money - (money//7)*7)
+        if children - count == 0:
+            if remainder == 0:
+                pass
+            else:
+                count = count - 1
+        elif remainder == 3 and (children - count) == 1:
+            count = count - 1
+        elif remainder / (children - count) > 7:
+            count = count - 1
+
+        return count
